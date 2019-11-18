127c0310d8245dce04cd249a72cfcaff12e2f604
Flypaopao <1147982831@qq.com>
2019-11-18 23:51:37 +0800

    搭建elasticsearch环境并部署

--- tensquare_search/src/main/java/com/tensquare/search/dao/ArticleDao.java
+++ tensquare_search/src/main/java/com/tensquare/search/dao/ArticleDao.java
@@ -1,7 +1,18 @@
 package com.tensquare.search.dao;
 
 import com.tensquare.search.pojo.Article;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
 public interface ArticleDao extends ElasticsearchRepository<Article,String> {
+
+    /**
+     * 根据标题或者内容模糊分页查询
+     * @param title
+     * @param content
+     * @param pageable
+     * @return
+     */
+    Page<Article> findByTitleOrContentLike(String title, String content, Pageable pageable);
 }
--- tensquare_search/src/main/java/com/tensquare/search/service/ArticleService.java
+++ tensquare_search/src/main/java/com/tensquare/search/service/ArticleService.java
@@ -3,6 +3,9 @@ package com.tensquare.search.service;
 import com.tensquare.search.dao.ArticleDao;
 import com.tensquare.search.pojo.Article;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import util.IdWorker;
 
@@ -18,8 +21,13 @@ public class ArticleService {
      * 增加文章
      * @param article
      */
-    public void add(Article article){
+    public void save(Article article){
         articleDao.save(article);
     }
 
+
+    public Page<Article> findByKey(String keywords, int page, int size) {
+        Pageable pageable = PageRequest.of(page-1,size);
+        return articleDao.findByTitleOrContentLike(keywords,keywords,pageable);
+    }
 }
