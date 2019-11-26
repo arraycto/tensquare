a3f67409cddb74bf0ea1da84b5322f577b16dc1a
Flypaopao <1147982831@qq.com>
2019-11-26 23:52:27 +0800

    搭建elasticsearch环境并部署

---
+++ tensquare_rabbitmqtest/src/main/java/com/tensquare/rabbit/RabbitApplication.java
@@ -0,0 +1,14 @@
+package com.tensquare.rabbit;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class RabbitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitApplication.class, args);
+	}
+
+
+}
